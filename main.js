81db925246cbd12c22f20ebbbcca6e6537071e91
Ahmad Zulfi <mymail.zulfi@gmail.com>
2025-04-12 21:26:26 +0700

    update main.js

--- main.js
+++ main.js
@@ -6,23 +6,42 @@ addTaskButton.addEventListener("click", function () {
   } else {
     let listTask = document.querySelector(".list-group"); // Mengambil elemen list tugas
     let listTaskHTML = listTask.innerHTML; // Mengambil isi list tugas
-    listTaskHTML += `<li class="list-group-item">
-            <input
-              class="form-check-input me-1"
-              type="checkbox"
-              value=""
-              id="thirdCheckboxStretched"
-            />
-            <label
-              class="form-check-label stretched-link"
-              >${inputTask.value}</label
-            >
-          </li>`; // Menambahkan HTML untuk tugas baru
-    listTask.innerHTML = listTaskHTML; // Menambahkan tugas baru ke dalam list
+    listTaskHTML += `<li class="list-group-item d-flex justify-content-between">
+                <div>
+                <input
+                  class="form-check-input me-1"
+                  type="checkbox"/>
+                <label>${inputTask.value}</label>
+                </div>
+                <button class="badge btn btn-danger border-0 btn-hapus">Delete</button>
+              </li>`; // Menambahkan HTML untuk tugas baru
+    listTask.innerHTML = listTaskHTML;
+    console.log("Task added '", inputTask.value, "'"); // Tampilkan perubahan ke halaman
     inputTask.value = ""; // Mengosongkan input setelah submit tugas
     inputTask.focus(); // Mengembalikan fokus typing ke input tanpa klik
+  }
 
-    let checkboxes = document.querySelectorAll(".form-check-input");
-    for (let i = 0; i < checkboxes.length; i++) {}
+  let checkboxes = document.querySelectorAll(".form-check-input");
+  for (let i = 0; i < checkboxes.length; i++) {
+    const input = checkboxes[i];
+    input.addEventListener("change", function () {
+      console.log("Checkbox changed");
+      let taskSpan = input.nextElementSibling;
+      taskSpan.classList.toggle("text-decoration-line-through");
+    });
+  }
+
+  let deleteButtons = document.querySelectorAll(".btn-hapus"); // Mengambil semua tombol hapus
+  for (let x = 0; x < deleteButtons.length; x++) {
+    const deleteBut = deleteButtons[x];
+    deleteBut.addEventListener("click", function () {
+      this.parentElement.remove(); // Menghapus tugas saat tombol hapus diklik
+    });
+  }
+});
+
+inputTask.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTaskButton.click(); // seolah tombol diklik
   }
 });
